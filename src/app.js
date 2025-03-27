1232dd9ee132c6f21dbc08d47e8252c9482d5e47
Dixthor <carlosvillalobos9704@gmail.com>
2025-03-27 03:42:57 +0000

    Termine el proyecto, falta CSS pero tiene boton

--- src/app.js
+++ src/app.js
@@ -9,10 +9,13 @@ window.onload = function() {
   let pronoun = ['the', 'our', 'they'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
+  let mostrarDominio = document.getElementById('dom');
+  let dominios = " ";
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
+        dominios += pronoun[i] + adj[j] + noun[k] + ".com <br>";
         console.log(pronoun[i] + adj[j] + noun[k] + ".com");
         
         
@@ -21,4 +24,5 @@ window.onload = function() {
     }
     
   }
+  mostrarDominio.innerHTML = dominios;
 };
